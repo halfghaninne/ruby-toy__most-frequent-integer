275b9d8a77522b472055e6b9b140e414120f3d94
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-01-14 11:25:55 -0600

    partially working method. does not account for all scenarios. notes in code comments.

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,23 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+  counts = Hash.new(0)
+  
+  arr.each do |int|
+    counts[int] += 1
+  end
+  # returns a hash with the count for each value in the array
+  
+  freq = counts.values
+  # returns array of just the frequencies.
+  
+  if freq.uniq.length > 1
+    freq.max
+    counts.key(freq.max)
+  else
+    nil
+  end
+  
 end
+
+# note to self: method does not yet account for a situation like [2,2,1,1,0]
